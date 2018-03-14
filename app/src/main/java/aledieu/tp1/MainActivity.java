42ff0ae097aa57677fedda2809f1de904d34f3d3
Alcalimex <ledieu.aurelien@gmail.com>
2018-03-14 16:10:43 +0100

    [Add] Console log when clicking on "Ok" button and "Name" input

--- app/src/main/java/aledieu/tp1/MainActivity.java
+++ app/src/main/java/aledieu/tp1/MainActivity.java
@@ -2,12 +2,39 @@ package aledieu.tp1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_relative);
+
+        Button button = findViewById(R.id.btnOK);
+        EditText editText = findViewById(R.id.inputNom);
+
+        button.setOnClickListener(this);
+        editText.setOnClickListener(this);
+    }
+
+
+    //Fonction permettant de réaliser les actions après un click sur un bouton
+    public void alerter(String message) {
+        Log.i("DWM", message);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnOK:
+                alerter("Clic sur bouton");
+                break;
+            case R.id.inputNom:
+                alerter("Click sur input");
+                break;
+        }
     }
 }
